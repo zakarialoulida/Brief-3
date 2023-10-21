a496dae0df5b2c28d15099ac7f2d0098075d83a8
zakaria loulida <zakarialoulida92@gmail.com>
2023-10-21 18:19:21 +0100

    FAQs réalisée

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const icons = document.getElementById("icons");
 const nav = document.getElementById("nav");
+/*const faqs=document.querySelectorAll(".faq")*/
 
 icons.addEventListener("click", () => {
     nav.classList.toggle("active");
+});
+/*faqs.forEach(faq =>{
+    faq.addEventListener("click",()=>
+    {faq.classList.toggle("activ");
+    });
+});*/
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    const question = faq.querySelector(".question");
+    const answer = faq.querySelector(".answer");
+
+    question.addEventListener("click", () => {
+        faq.classList.toggle("active");
+
+        if (faq.classList.contains("active")) {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        } else {
+            answer.style.maxHeight = "0";
+        }
+    });
 });
